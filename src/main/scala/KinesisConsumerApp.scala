4a81a06e75200a9452058751500288a5a5d2c44e
mprescha <mirko.prescha@immobilienscout24.de>
2016-09-08 16:29:09 +0200

    implement checkpointing

--- src/main/scala/KinesisConsumerApp.scala
+++ src/main/scala/KinesisConsumerApp.scala
@@ -4,13 +4,14 @@ import java.net.InetAddress
 import java.util.UUID
 
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker.{Worker, KinesisClientLibConfiguration}
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.{KinesisClientLibConfiguration, Worker}
 import com.amazonaws.services.kinesis.metrics.impl.NullMetricsFactory
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
 object KinesisConsumerApp extends App{
-  System.setProperty("org.slf4j.simplelogger.defaultlog", "trace")
+  System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE")
+
   implicit val logger = Logger(LoggerFactory.getLogger("KinesisConsumerApp"))
   logger.info ("start reading App Config")
   val kinesisConsumerConfig = new KinesisConsumerConfig
@@ -24,7 +25,6 @@ object KinesisConsumerApp extends App{
     new ProfileCredentialsProvider(),
     workerId
   ).withKinesisEndpoint(kinesisConsumerConfig.streamEndpoint)
-
   logger.info (s"Created KinesisClientLibConfiguration for Application ${kinesisConsumerConfig.applicationName} consuming ${kinesisConsumerConfig.streamName}")
 
   val recordProcessorFactory = new KinesisRecordProcessorFactory()
--- src/main/scala/KinesisRecordProcessor.scala
+++ src/main/scala/KinesisRecordProcessor.scala
@@ -1,6 +1,8 @@
 package de.is24.data.dwh.metricaggregator
 
 import java.util
+import com.amazonaws.services.kinesis.clientlibrary.exceptions.{InvalidStateException, ThrottlingException, ShutdownException}
+
 import scala.collection.JavaConversions._
 import com.amazonaws.services.kinesis.clientlibrary.interfaces.{IRecordProcessor, IRecordProcessorCheckpointer}
 import com.amazonaws.services.kinesis.clientlibrary.types.ShutdownReason
@@ -29,13 +31,16 @@ class KinesisRecordProcessor (implicit logger: Logger) extends IRecordProcessor{
 
 
   override def processRecords(records: util.List[Record], checkpointer: IRecordProcessorCheckpointer): Unit = {
-    logger.debug(s"Processing ${records} records from ${kinesisShardId}")
+    logger.debug(s"Processing ${records.size} records from ${kinesisShardId}")
 
     for (record <- records) {
       try {
-        logger.trace(s"Sequence number: ${record.getSequenceNumber}")
-        logger.trace(s"Record data: ${record.getData.array.toString}")
         logger.trace(s"Partition key: ${record.getPartitionKey}")
+        logger.trace(s"Sequence number: ${record.getSequenceNumber}")
+        logger.trace(s"ApproximateArrivalTimestamp: ${record.getApproximateArrivalTimestamp}")
+        logger.trace(s"Record data: ${record.getData.toString}")
+     //   checkpoint(checkpointer)
+
       } catch {
         case t: Throwable =>
           println(s"Caught throwable while processing record $record")
@@ -48,33 +53,33 @@ class KinesisRecordProcessor (implicit logger: Logger) extends IRecordProcessor{
   override def shutdown(checkpointer: IRecordProcessorCheckpointer, reason: ShutdownReason): Unit = {
     logger.info (s"Shutting down record processor for shard: $kinesisShardId")
     if (reason == ShutdownReason.TERMINATE) {
-      ///checkpoint(checkpointer)
+      checkpoint(checkpointer)
       println("shutdown")
     }
   }
 
-//
-//  private def checkpoint(checkpointer: IRecordProcessorCheckpointer) = {
-//    println(s"Checkpointing shard $kinesisShardId")
-//    breakable { for (i <- 0 to NUM_RETRIES-1) {
-//      try {
-//        checkpointer.checkpoint()
-//        break
-//      } catch {
-//        case se: ShutdownException =>
-//          println("Caught shutdown exception, skipping checkpoint.", se)
-//        case e: ThrottlingException =>
-//          if (i >= (NUM_RETRIES - 1)) {
-//            println(s"Checkpoint failed after ${i+1} attempts.", e)
-//          } else {
-//            println(s"Transient issue when checkpointing - attempt ${i+1} of "
-//              + NUM_RETRIES, e)
-//          }
-//        case e: InvalidStateException =>
-//          println("Cannot save checkpoint to the DynamoDB table used by " +
-//            "the Amazon Kinesis Client Library.", e)
-//      }
-//      Thread.sleep(BACKOFF_TIME_IN_MILLIS)
-//    }
-//    } }
+
+  private def checkpoint(checkpointer: IRecordProcessorCheckpointer) = {
+    logger.debug(s"Checkpointing shard $kinesisShardId")
+    for (i <- 0 to NUM_RETRIES - 1) {
+      try {
+        checkpointer.checkpoint()
+      } catch {
+        case se: ShutdownException =>
+          logger.error("Caught shutdown exception, skipping checkpoint.", se)
+        case e: ThrottlingException =>
+          if (i >= (NUM_RETRIES - 1)) {
+            logger.error(s"Checkpoint failed after ${i + 1} attempts.", e)
+          } else {
+            logger.warn(s"Transient issue when checkpointing - attempt ${i + 1} of "
+              + NUM_RETRIES, e)
+            Thread.sleep(BACKOFF_TIME_IN_MILLIS)
+          }
+        case e: InvalidStateException =>
+          logger.error("Cannot save checkpoint to the DynamoDB table used by " +
+            "the Amazon Kinesis Client Library.", e)
+      }
+
+    }
+  }
 }
